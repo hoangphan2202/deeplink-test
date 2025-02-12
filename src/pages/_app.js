3d92d887f200c98b95ed6e4bc6de3b0d5a8e772b
Hoang Phan <vanhoang22021997@gmail.com>
2025-02-12 09:52:20 +0700

    testttt

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,45 @@
-import '../globals.scss'
+import "../globals.scss";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#86FF00" />
+        <meta
+          name="description"
+          content="WOW Sport - Your Ultimate Sports Companion"
+        />
+        <title>WOW Sport</title>
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* PWA primary color */}
+        <meta name="theme-color" content="#86FF00" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="WOW Sport" />
+        <meta
+          property="og:description"
+          content="WOW Sport - Your Ultimate Sports Companion"
+        />
+        <meta property="og:image" content="/og-image.jpg" />
+        <meta name="apple-itunes-app" content="app-id=<ITUNES_ID>" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="WOW Sport" />
+        <meta
+          name="twitter:description"
+          content="WOW Sport - Your Ultimate Sports Companion"
+        />
+        <meta name="twitter:image" content="/og-image.jpg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,9 +1,16 @@
-import  { useEffect } from 'react';
+import { useEffect } from "react";
 
 const SignUp = () => {
-    useEffect(() => {
-        window.location.href = 'https://play.google.com/store/apps/details?id=app.pibase.co';
-    }, []);
+  useEffect(() => {
+    window.location.href =
+      "https://play.google.com/store/apps/details?id=vn.f5seconds.wowsport.development";
+  }, []);
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+    </div>
+  );
 };
 
 export default SignUp;
